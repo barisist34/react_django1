c8dc33ae3fcc0138ff22c582e4f97ce6b3c191c1
barisist34 <uyarbaris@gmail.com>
2024-05-04 17:10:40 +0300

    240505-- views.py de ReactView(APIView) get() metodu random bir kayıt oluşturup, App.js üzerinden setInterval componentDidMount 2 saniye tekrarlı yöntemiyle tarayıcı üzerinde peryodik kayıtlar görüntülenmektedir.

--- react_django1app/views.py
+++ react_django1app/views.py
@@ -3,17 +3,35 @@ from rest_framework.views import APIView
 from . models import *
 from . serializer import *
 from rest_framework.response import Response
+from threading import Timer
+import time
+from random import randint
 
 
 class ReactView(APIView):
-    def get(self, request):
-        output = [{"employee": output.employee, "department": output.department}
-                  for output in React.objects.all()[::-1]
-                  ]
-        return Response(output)
+    # def get(self, request):
+
+    #     output = [{"employee": output.employee, "department": output.department}
+    #               for output in React.objects.all()[::-1]
+    #               ]
+    #     # output = [{"employee": output.employee, "department": output.department}
+    #     #           for output in React.objects.all()[::-1]
+    #     #           ]
+    #     return Response(output)
 
     def post(self, request):
         serializer = ReactSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+    def get(self,request):
+        new_record= React.objects.create(employee=f"Worker-{str(randint(1,50))}",department=f"Department-{str(randint(1,50))}")
+    #     for i in range(100):
+        output = [{"employee": output.employee, "department": output.department}
+                for output in React.objects.all()[::-1]
+                ]
+    #         output = [{"employee": str(i) , "department": "döngü 100"}]
+    #         time.sleep(2)
+        print(f"timer_random {output}")
+        return Response(output)
+
